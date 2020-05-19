926f21fa7ed3c00e4f607e6c353069d1096e0c63
Anton <goldtom1423@gmail.com>
2020-05-20 00:11:08 +0200

    Add files
    Update 1.3.0

---
+++ app.py
@@ -0,0 +1,151 @@
+from PyQt5.QtWidgets import QApplication, QMessageBox, QDialog
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from gui_about import Ui_AboutWindow
+from gui_main import Ui_MainWindow
+from PyQt5.QtGui import QCursor
+import webbrowser
+import subprocess
+import sys
+
+
+app_version = "1.3.0"
+
+
+class Logic():
+    def __init__(self):
+        about.label_version.setText(f"Version {app_version}")
+        self.installed_apps = []
+        self.checkbox_dict = {
+            ui.checkBox: "*Microsoft.Microsoft3DViewer*", ui.checkBox_2: "*Microsoft.WindowsAlarms*", ui.checkBox_3: "*Microsoft.WindowsCalculator*",
+            ui.checkBox_4: "*microsoft.windowscommunicationsapps*", ui.checkBox_5: "*Microsoft.WindowsCamera*", ui.checkBox_6: "*Microsoft.GetHelp*",
+            ui.checkBox_7: "*Microsoft.Getstarted*", ui.checkBox_8: "*Microsoft.ZuneMusic*", ui.checkBox_9: "*Microsoft.WindowsMaps*",
+
+            ui.checkBox_10: "*Microsoft.Messaging*", ui.checkBox_11: "*Microsoft.BingFinance*", ui.checkBox_12: "*Microsoft.ZuneVideo*",
+            ui.checkBox_13: "*Microsoft.BingNews*", ui.checkBox_14: "*Microsoft.MicrosoftOfficeHub*", ui.checkBox_15: "*Microsoft.Office.OneNote*",
+            ui.checkBox_16: "*Microsoft.MSPaint*", ui.checkBox_17: "*Microsoft.People*", ui.checkBox_18: "*Microsoft.Windows.Photos*",
+
+            ui.checkBox_19: "*Microsoft.SkypeApp*", ui.checkBox_20: "*Microsoft.MicrosoftSolitaireCollection*", ui.checkBox_21: "*Microsoft.BingSports*",
+            ui.checkBox_22: "*Microsoft.WindowsStore*", ui.checkBox_23: "*Microsoft.WindowsSoundRecorder*", ui.checkBox_24: "*Microsoft.BingWeather*",
+            ui.checkBox_25: "*Microsoft.WindowsFeedbackHub*", ui.checkBox_26: "*xbox* | Where-Object {$_.name -notmatch 'xboxgamecallableui'}", ui.checkBox_27: "*Microsoft.YourPhone*",
+        }
+        ui.button_deselect_all.setDisabled(True)
+        ui.button_uninstall.setDisabled(True)
+
+    def logic_setup(self):
+        ui.actionRefresh.triggered.connect(self.app_refresh)
+        ui.actionHomepage.triggered.connect(self.app_homepage)
+        ui.actionAbout.triggered.connect(self.app_about)
+        ui.actionQuit.triggered.connect(self.app_quit)
+        ui.button_uninstall.clicked.connect(self.uninstall)
+        ui.button_select_all.clicked.connect(self.select_all)
+        ui.button_deselect_all.clicked.connect(self.deselect_all)
+        for i in self.checkbox_dict:
+            i.clicked.connect(self.enable_buttons)
+        self.start_thread()
+
+    def start_thread(self):
+        self.worker = Worker()
+        self.worker.start()
+        self.worker.finished.connect(self.thread_finished)
+        self.worker.progress_signal.connect(self.update_progress)
+
+    def thread_finished(self):
+        ui.progressbar.hide()
+        ui.progressbar.setValue(0)
+        QApplication.setOverrideCursor(QCursor())
+        ui.label_info.setText('Select the default Windows 10 apps to uninstall:')
+        ui.actionRefresh.setDisabled(False)
+        ui.button_select_all.setDisabled(False)
+        for i in self.installed_apps:
+            i.setEnabled(True)
+
+    def update_progress(self, progress):
+        ui.progressbar.setValue(progress)
+
+    def enable_buttons(self):
+        if any(i.isChecked() for i in self.installed_apps):
+            ui.button_uninstall.setDisabled(False)
+            ui.button_deselect_all.setDisabled(False)
+        else:
+            ui.button_deselect_all.setDisabled(True)
+            ui.button_uninstall.setDisabled(True)
+
+        if all(i.isChecked() for i in self.installed_apps):
+            ui.button_select_all.setDisabled(True)
+        else:
+            ui.button_select_all.setDisabled(False)
+
+    def app_refresh(self):
+        self.start_thread()
+
+    def app_homepage(self):
+        webbrowser.open_new('https://github.com/Teraskull/PyDebloatX')
+
+    def app_about(self):
+        about.setWindowModality(Qt.ApplicationModal)
+        about.show()
+
+    def app_quit(self):
+        buttonReply = QMessageBox.question(ui, 'PyDebloatX', f"Quit PyDebloatX?", QMessageBox.Yes | QMessageBox.No)
+        if buttonReply == QMessageBox.Yes:
+            app.quit()
+
+    def select_all(self):
+        for i in self.installed_apps:
+            if not i.isChecked():
+                i.setChecked(True)
+        self.enable_buttons()
+
+    def deselect_all(self):
+        for i in self.installed_apps:
+            if i.isChecked():
+                i.setChecked(False)
+        self.enable_buttons()
+
+    def uninstall(self):
+        j = 0
+        for i in self.installed_apps:
+            if i.isChecked():
+                j += 1
+        buttonReply = QMessageBox.question(ui, 'PyDebloatX', f"Uninstall {j} selected app(s)?", QMessageBox.Yes | QMessageBox.No)
+        if buttonReply == QMessageBox.Yes:
+            for i in self.checkbox_dict:
+                if i.isChecked():
+                    subprocess.Popen(f'powershell Get-AppxPackage {self.checkbox_dict[i]} | Remove-AppxPackage')
+                    i.setChecked(False)
+                    i.setEnabled(False)
+            self.deselect_all()
+            QMessageBox.information(ui, 'PyDebloatX', f"Successfully uninstalled {j} app(s).", QMessageBox.Ok)
+
+
+class Worker(QThread):
+    end_signal = pyqtSignal()
+    progress_signal = pyqtSignal(int)
+
+    def run(self):
+        ui.progressbar.show()
+        ui.actionRefresh.setDisabled(True)
+        ui.button_select_all.setDisabled(True)
+        QApplication.setOverrideCursor(QCursor(Qt.BusyCursor))
+        progress = 100 / 27
+        for i in logic.checkbox_dict:
+            i.setEnabled(False)
+        for i in logic.checkbox_dict:
+            x = subprocess.Popen(["powershell", f"(Get-AppxPackage {logic.checkbox_dict[i]}) -and ($true)"], stdout=subprocess.PIPE).communicate()[0]
+            progress += 100 / 27
+            self.progress_signal.emit(int(progress))
+            if x.decode().strip() == "True":
+                logic.installed_apps.append(i)
+        self.end_signal.emit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    about = Ui_AboutWindow()
+    about.setupUi()
+    ui = Ui_MainWindow()
+    ui.setupUi()
+    logic = Logic()
+    logic.logic_setup()
+    ui.show()
+    sys.exit(app.exec_())
---
+++ gui_about.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtCore import Qt, QRect, QCoreApplication, QMetaObject
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtGui import QIcon, QFont
+
+
+class Ui_AboutWindow(QMainWindow):
+    def __init__(self):
+        super(Ui_AboutWindow, self).__init__()
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.WindowCloseButtonHint)
+        # self.setWindowIcon(QIcon('icon.ico'))
+        self.setFixedSize(240, 130)
+
+    def setupUi(self):
+        self.centralwidget = QWidget(self)
+        self.verticalLayoutWidget = QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QRect(10, 10, 221, 51))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.label_name = QLabel(self.verticalLayoutWidget)
+        font = QFont()
+        font.setPointSize(13)
+        font.setStyleStrategy(QFont.PreferAntialias)
+        self.label_name.setFont(font)
+        self.label_name.setLayoutDirection(Qt.LeftToRight)
+        self.verticalLayout.addWidget(self.label_name, 0, Qt.AlignHCenter)
+        self.label_version = QLabel(self.verticalLayoutWidget)
+        font = QFont()
+        font.setPointSize(10)
+        self.label_version.setFont(font)
+        self.label_version.setLayoutDirection(Qt.LeftToRight)
+        self.verticalLayout.addWidget(self.label_version, 0, Qt.AlignHCenter)
+        self.verticalLayoutWidget_2 = QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QRect(10, 80, 221, 41))
+        self.verticalLayout_2 = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label_copyright = QLabel(self.verticalLayoutWidget_2)
+        font = QFont()
+        font.setPointSize(10)
+        self.label_copyright.setFont(font)
+        self.label_copyright.setLayoutDirection(Qt.LeftToRight)
+        self.verticalLayout_2.addWidget(self.label_copyright, 0, Qt.AlignHCenter)
+        self.label_author = QLabel(self.verticalLayoutWidget_2)
+        font = QFont()
+        font.setPointSize(10)
+        self.label_author.setFont(font)
+        self.label_author.setLayoutDirection(Qt.LeftToRight)
+        self.verticalLayout_2.addWidget(self.label_author, 0, Qt.AlignHCenter)
+        self.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi()
+        QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self):
+        _translate = QCoreApplication.translate
+        self.setWindowTitle(_translate("MainWindow", "About"))
+        self.label_name.setText(_translate("MainWindow", "PyDebloatX"))
+        self.label_version.setText(_translate("MainWindow", ""))
+        self.label_copyright.setText(_translate("MainWindow", "Copyright © 2020 by"))
+        self.label_author.setText(_translate("MainWindow", "Anton Grouchtchak."))
