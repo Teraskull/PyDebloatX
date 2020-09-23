7dcd629e5434dc2f48361116a0a40827ec7a58c1
Anton <goldtom1423@gmail.com>
2020-09-24 01:11:00 +0200

    Optimize app dictionary
    Update app sizes.

--- pydebloatx/app.py
+++ pydebloatx/app.py
@@ -31,47 +31,41 @@ class Logic():
         self.app_genitive_singular = 'apps'
         self.app_genitive_plural = 'apps'
         self.size_available_text = 'MB of of disk space will be available.'
-        self.checkbox_dict = {
-            ui.checkBox: "*Microsoft.3DBuilder*", ui.checkBox_2: "*Microsoft.Microsoft3DViewer*", ui.checkBox_3: "*Microsoft.WindowsAlarms*",
-            ui.checkBox_4: "*Microsoft.WindowsCalculator*", ui.checkBox_5: "*microsoft.windowscommunicationsapps*", ui.checkBox_6: "*Microsoft.WindowsCamera*",
-            ui.checkBox_7: "*Microsoft.GetHelp*", ui.checkBox_8: "*Microsoft.ZuneMusic*", ui.checkBox_9: "*Microsoft.WindowsMaps*",
 
-            ui.checkBox_10: "*Microsoft.Messaging*", ui.checkBox_11: "*Microsoft.BingFinance*", ui.checkBox_12: "*Microsoft.ZuneVideo*",
-            ui.checkBox_13: "*Microsoft.BingNews*", ui.checkBox_14: "*Microsoft.MicrosoftOfficeHub*", ui.checkBox_15: "*Microsoft.Office.OneNote*",
-            ui.checkBox_16: "*Microsoft.MSPaint*", ui.checkBox_17: "*Microsoft.People*", ui.checkBox_18: "*Microsoft.Windows.Photos*",
+        self.apps_dict = {
+            ui.checkBox: {"name": "*Microsoft.3DBuilder*", "link": "/?PFN=Microsoft.3DBuilder_8wekyb3d8bbwe", "size": 35.02},
+            ui.checkBox_2: {"name": "*Microsoft.Microsoft3DViewer*", "link": "/?PFN=Microsoft.Microsoft3DViewer_8wekyb3d8bbwe", "size": 121.46},
+            ui.checkBox_3: {"name": "*Microsoft.WindowsAlarms*", "link": "/?PFN=Microsoft.WindowsAlarms_8wekyb3d8bbwe", "size": 11.87},
+            ui.checkBox_4: {"name": "*Microsoft.WindowsCalculator*", "link": "/?PFN=Microsoft.WindowsCalculator_8wekyb3d8bbwe", "size": 14.27},
+            ui.checkBox_5: {"name": "*microsoft.windowscommunicationsapps*", "link": "/?PFN=Microsoft.windowscommunicationsapps_8wekyb3d8bbwe", "size": 245.78},
+            ui.checkBox_6: {"name": "*Microsoft.WindowsCamera*", "link": "/?PFN=Microsoft.WindowsCamera_8wekyb3d8bbwe", "size": 49.05},
+            ui.checkBox_7: {"name": "*Microsoft.GetHelp*", "link": "/?PFN=Microsoft.Gethelp_8wekyb3d8bbwe", "size": 11.89},
+            ui.checkBox_8: {"name": "*Microsoft.ZuneMusic*", "link": "/?PFN=Microsoft.ZuneMusic_8wekyb3d8bbwe", "size": 50.34},
+            ui.checkBox_9: {"name": "*Microsoft.WindowsMaps*", "link": "/?PFN=Microsoft.WindowsMaps_8wekyb3d8bbwe", "size": 39.80},
 
-            ui.checkBox_19: "*Microsoft.SkypeApp*", ui.checkBox_20: "*Microsoft.MicrosoftSolitaireCollection*", ui.checkBox_21: "*Microsoft.BingSports*",
-            ui.checkBox_22: "*Microsoft.Getstarted*", ui.checkBox_23: "*Microsoft.WindowsSoundRecorder*", ui.checkBox_24: "*Microsoft.BingWeather*",
-            ui.checkBox_25: "*Microsoft.WindowsFeedbackHub*", ui.checkBox_26: "*xbox* | Where-Object {$_.name -notmatch 'xboxgamecallableui'}", ui.checkBox_27: "*Microsoft.YourPhone*",
+            ui.checkBox_10: {"name": "*Microsoft.Messaging*", "link": "/?PFN=Microsoft.Messaging_8wekyb3d8bbwe", "size": 30.08},
+            ui.checkBox_11: {"name": "*Microsoft.BingFinance*", "link": "?productId=9WZDNCRFHV4V", "size": 32.82},
+            ui.checkBox_12: {"name": "*Microsoft.ZuneVideo*", "link": "/?PFN=Microsoft.ZuneVideo_8wekyb3d8bbwe", "size": 51.80},
+            ui.checkBox_13: {"name": "*Microsoft.BingNews*", "link": "?productId=9WZDNCRFHVFW", "size": 35.29},
+            ui.checkBox_14: {"name": "*Microsoft.MicrosoftOfficeHub*", "link": "/?PFN=Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe", "size": 30.21},
+            ui.checkBox_15: {"name": "*Microsoft.Office.OneNote*", "link": "/?PFN=Microsoft.Office.OneNote_8wekyb3d8bbwe", "size": 156.01},
+            ui.checkBox_16: {"name": "*Microsoft.MSPaint*", "link": "/?PFN=Microsoft.MSPaint_8wekyb3d8bbwe", "size": 65.79},
+            ui.checkBox_17: {"name": "*Microsoft.People*", "link": "/?PFN=Microsoft.People_8wekyb3d8bbwe", "size": 31.97},
+            ui.checkBox_18: {"name": "*Microsoft.Windows.Photos*", "link": "/?PFN=Microsoft.Windows.Photos_8wekyb3d8bbwe", "size": 346.04},
+
+            ui.checkBox_19: {"name": "*Microsoft.SkypeApp*", "link": "/?PFN=Microsoft.SkypeApp_kzf8qxf38zg5c", "size": 237.25},
+            ui.checkBox_20: {"name": "*Microsoft.MicrosoftSolitaireCollection*", "link": "/?PFN=Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe", "size": 134.37},
+            ui.checkBox_21: {"name": "*Microsoft.BingSports*", "link": "?productId=9WZDNCRFHVH4", "size": 30.92},
+            ui.checkBox_22: {"name": "*Microsoft.Getstarted*", "link": "/?PFN=Microsoft.Getstarted_8wekyb3d8bbwe", "size": 19.53},
+            ui.checkBox_23: {"name": "*Microsoft.WindowsSoundRecorder*", "link": "/?PFN=Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe", "size": 12.40},
+            ui.checkBox_24: {"name": "*Microsoft.BingWeather*", "link": "/?PFN=Microsoft.BingWeather_8wekyb3d8bbwe", "size": 32.32},
+            ui.checkBox_25: {"name": "*Microsoft.WindowsFeedbackHub*", "link": "/?PFN=Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe", "size": 35.02},
+            ui.checkBox_26: {"name": "*xbox* | Where-Object {$_.name -notmatch 'xboxgamecallableui'}", "link": "/?PFN=Microsoft.XboxApp_8wekyb3d8bbwe", "size": 55.91},
+            ui.checkBox_27: {"name": "*Microsoft.YourPhone*", "link": "/?PFN=Microsoft.YourPhone_8wekyb3d8bbwe", "size": 280.27}
         }
-        self.link_dict = {
-            ui.checkBox: "/?PFN=Microsoft.3DBuilder_8wekyb3d8bbwe", ui.checkBox_2: "/?PFN=Microsoft.Microsoft3DViewer_8wekyb3d8bbwe", ui.checkBox_3: "/?PFN=Microsoft.WindowsAlarms_8wekyb3d8bbwe",
-            ui.checkBox_4: "/?PFN=Microsoft.WindowsCalculator_8wekyb3d8bbwe", ui.checkBox_5: "/?PFN=Microsoft.windowscommunicationsapps_8wekyb3d8bbwe", ui.checkBox_6: "/?PFN=Microsoft.WindowsCamera_8wekyb3d8bbwe",
-            ui.checkBox_7: "/?PFN=Microsoft.Gethelp_8wekyb3d8bbwe", ui.checkBox_8: "/?PFN=Microsoft.ZuneMusic_8wekyb3d8bbwe", ui.checkBox_9: "/?PFN=Microsoft.WindowsMaps_8wekyb3d8bbwe",
 
-            ui.checkBox_10: "/?PFN=Microsoft.Messaging_8wekyb3d8bbwe", ui.checkBox_11: "?productId=9WZDNCRFHV4V", ui.checkBox_12: "/?PFN=Microsoft.ZuneVideo_8wekyb3d8bbwe",
-            ui.checkBox_13: "?productId=9WZDNCRFHVFW", ui.checkBox_14: "/?PFN=Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe", ui.checkBox_15: "/?PFN=Microsoft.Office.OneNote_8wekyb3d8bbwe",
-            ui.checkBox_16: "/?PFN=Microsoft.MSPaint_8wekyb3d8bbwe", ui.checkBox_17: "/?PFN=Microsoft.People_8wekyb3d8bbwe", ui.checkBox_18: "/?PFN=Microsoft.Windows.Photos_8wekyb3d8bbwe",
-
-            ui.checkBox_19: "/?PFN=Microsoft.SkypeApp_kzf8qxf38zg5c", ui.checkBox_20: "/?PFN=Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe", ui.checkBox_21: "?productId=9WZDNCRFHVH4",
-            ui.checkBox_22: "/?PFN=Microsoft.Getstarted_8wekyb3d8bbwe", ui.checkBox_23: "/?PFN=Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe", ui.checkBox_24: "/?PFN=Microsoft.BingWeather_8wekyb3d8bbwe",
-            ui.checkBox_25: "/?PFN=Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe", ui.checkBox_26: "/?PFN=Microsoft.XboxApp_8wekyb3d8bbwe", ui.checkBox_27: "/?PFN=Microsoft.YourPhone_8wekyb3d8bbwe",
-        }
-        self.size_dict = {
-            ui.checkBox: 35.02, ui.checkBox_2: 121.46, ui.checkBox_3: 11.87,
-            ui.checkBox_4: 9.27, ui.checkBox_5: 230.90, ui.checkBox_6: 49.05,
-            ui.checkBox_7: 11.89, ui.checkBox_8: 50.34, ui.checkBox_9: 29.93,
-
-            ui.checkBox_10: 30.08, ui.checkBox_11: 31.92, ui.checkBox_12: 51.80,
-            ui.checkBox_13: 33.70, ui.checkBox_14: 29.97, ui.checkBox_15: 156.01,
-            ui.checkBox_16: 65.79, ui.checkBox_17: 31.97, ui.checkBox_18: 346.04,
-
-            ui.checkBox_19: 104.70, ui.checkBox_20: 134.37, ui.checkBox_21: 30.92,
-            ui.checkBox_22: 16.62, ui.checkBox_23: 12.40, ui.checkBox_24: 30.59,
-            ui.checkBox_25: 35.02, ui.checkBox_26: 119.06, ui.checkBox_27: 64.59,
-        }
         ui.progressbar.setValue(0)
-        ui.progressbar.setMaximum(len(self.checkbox_dict))
+        ui.progressbar.setMaximum(len(self.apps_dict))
         ui.button_uninstall.clicked.connect(self.uninstall)
         ui.button_select_all.clicked.connect(self.select_all)
         ui.button_deselect_all.clicked.connect(self.deselect_all)
@@ -86,15 +80,15 @@ class Logic():
         ui.quit_btn.clicked.connect(self.app_quit)
         ui.quit_bind.activated.connect(self.app_quit)
         about.button_quit_about.clicked.connect(about.close)
-        for i in self.checkbox_dict:
+        for i in self.apps_dict:
             i.clicked.connect(self.enable_buttons)
             with open("style.css", 'r') as file:
                 i.setStyleSheet(file.read())
 
         self.workerThread = QThread()
         self.thread_list = []
-        for item, i in enumerate(self.checkbox_dict):
-            self.thread_list.append(CheckApps(self.checkbox_dict, i))
+        for item, i in enumerate(self.apps_dict):
+            self.thread_list.append(CheckApps(self.apps_dict, i))
             self.thread_list[item].moveToThread(self.workerThread)
             self.thread_list[item].app_signal.connect(self.enable_installed)
             self.thread_list[item].progress_signal.connect(self.update_progress)
@@ -105,7 +99,7 @@ class Logic():
             self.is_link_menu = False
             ui.label_info.setText(self.main_title)
             ui.store_btn.setIcon(QIcon(':/icon/store_icon.png'))
-            for i in self.checkbox_dict:
+            for i in self.apps_dict:
                 i.setEnabled(False)
                 i.setChecked(False)
             for i in self.installed_apps:
@@ -121,7 +115,7 @@ class Logic():
             self.is_link_menu = True
             ui.label_info.setText(self.store_title)
             ui.store_btn.setIcon(QIcon(':/icon/back_icon.png'))
-            for i in self.checkbox_dict:
+            for i in self.apps_dict:
                 i.setEnabled(True)
                 i.setChecked(True)
             ui.button_select_all.hide()
@@ -136,7 +130,7 @@ class Logic():
             self.store_menu()
         self.installed_apps = []
         self.progress = 0
-        for i in self.checkbox_dict:
+        for i in self.apps_dict:
             i.setEnabled(False)
             i.setChecked(False)
         ui.label_refresh.show()
@@ -180,7 +174,7 @@ class Logic():
     def update_progress(self):
         self.progress += 1
         ui.progressbar.setValue(self.progress)
-        if self.progress >= len(self.checkbox_dict):
+        if self.progress >= len(self.apps_dict):
             self.thread_finished()
 
     def enable_buttons(self):
@@ -188,7 +182,7 @@ class Logic():
             self.total_size = 0
             for i in self.installed_apps:
                 if i.isChecked():
-                    self.total_size += self.size_dict[i]
+                    self.total_size += self.apps_dict[i]["size"]
                     ui.label_size.setText(f'{self.total_size:.2f} {self.size_text}')
             if any(i.isChecked() for i in self.installed_apps):
                 ui.button_uninstall.setDisabled(False)
@@ -209,10 +203,10 @@ class Logic():
                 ui.button_select_all.setDisabled(False)
                 ui.button_select_all.setIcon(QIcon(':/icon/check_icon.png'))
         else:
-            for i in self.link_dict:
+            for i in self.apps_dict:
                 if not i.isChecked():
                     i.setChecked(True)
-                    webbrowser.open_new(f"ms-windows-store://pdp{self.link_dict[i]}")
+                    webbrowser.open_new(f'ms-windows-store://pdp{self.apps_dict[i]["link"]}')
 
     @staticmethod
     def app_homepage():
@@ -282,9 +276,9 @@ class Logic():
         msg_confirm_result = msg_confirm.exec_()
 
         if msg_confirm_result == QMessageBox.Yes:
-            for i in self.checkbox_dict:
+            for i in self.apps_dict:
                 if i.isChecked():
-                    subprocess.Popen(["powershell", f"(Get-AppxPackage {self.checkbox_dict[i]} | Remove-AppxPackage)"], shell=True)
+                    subprocess.Popen(["powershell", f'(Get-AppxPackage {self.apps_dict[i]["name"]} | Remove-AppxPackage)'], shell=True)
                     i.setChecked(False)
                     i.setEnabled(False)
                     self.installed_apps.remove(i)
@@ -308,13 +302,13 @@ class CheckApps(QThread):
     progress_signal = pyqtSignal()
     app_signal = pyqtSignal(object)
 
-    def __init__(self, checkbox_dict, i):
+    def __init__(self, apps_dict, i):
         super().__init__()
-        self.checkbox_dict = checkbox_dict
+        self.apps_dict = apps_dict
         self.i = i
 
     def run(self):
-        x = subprocess.Popen(["powershell", f"(Get-AppxPackage {self.checkbox_dict[self.i]}) -and $?"], stdout=subprocess.PIPE, shell=True)
+        x = subprocess.Popen(["powershell", f'(Get-AppxPackage {self.apps_dict[self.i]["name"]}) -and $?'], stdout=subprocess.PIPE, shell=True)
         if x.communicate()[0].decode().strip() == "True":
             self.app_signal.emit(self.i)
         self.progress_signal.emit()
