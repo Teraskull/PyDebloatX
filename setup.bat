40bafc68a6d7a2af8a877d5be85447b6d187f0e6
Anton <goldtom1423@gmail.com>
2020-11-02 22:16:57 +0100

    Use Pyinstaller develop branch

--- setup.bat
+++ setup.bat
@@ -3,10 +3,17 @@ cls
 
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [1] Updating PyInstaller...
+echo [1] Installing/Updating PyInstaller (develop branch)...
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-pip install -U pyinstaller
+
+:update
+pip install pyinstaller==4.1.dev0
+if ERRORLEVEL 1 goto install
+goto build
+
+:install
+pip install https://github.com/pyinstaller/pyinstaller/archive/develop.tar.gz
 if ERRORLEVEL 1 goto errorupdate
 goto build
 
@@ -25,54 +32,12 @@ if ERRORLEVEL 1 goto errorbuild
 echo.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [3] Removing unused files...
+echo [3] Removing build files...
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 rmdir /s /q "./build/"
 del app.spec
-
-cd dist/app
-
-del _asyncio.pyd
-del _bz2.pyd
-del _ctypes.pyd
-del _decimal.pyd
-del _elementtree.pyd
-del _hashlib.pyd
-del _lzma.pyd
-del _multiprocessing.pyd
-del _overlapped.pyd
-del _queue.pyd
-del _ssl.pyd
-del _testcapi.pyd
-del _tkinter.pyd
-del pyexpat.pyd
-del unicodedata.pyd
-
-del d3dcompiler_47.dll
-del libcrypto-1_1.dll
-del libEGL.dll
-del libffi-7.dll
-del libGLESv2.dll
-del libssl-1_1.dll
-del MSVCP140.dll
-del opengl32sw.dll
-del Qt5DBus.dll
-del Qt5Network.dll
-del Qt5Qml.dll
-del Qt5QmlModels.dll
-del Qt5Quick.dll
-del Qt5Svg.dll
-del Qt5WebSockets.dll
-del tcl86t.dll
-del tk86t.dll
-del VCRUNTIME140.dll
-del VCRUNTIME140_1.dll
-
-rmdir /s /q "./lib2to3/"
-rmdir /s /q "./tcl/tzdata/"
-rmdir /s /q "./tcl\encoding/"
-rmdir /s /q "./PyQt5/Qt/translations/"
 
 echo.
 echo Done.
