e811ec081340fa9437a6aa6a9dc4b7111282d1d2
Anton <goldtom1423@gmail.com>
2021-01-07 23:27:32 +0100

    Update setup-portable.bat

--- setup-portable.bat
+++ setup-portable.bat
@@ -1,28 +1,46 @@
 @echo off
+Color 07
 cls
+
+set steps=6
+
+
+:createenv
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo [1/%steps%] Creating virtual environment...
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pip install -U virtualenv
+virtualenv env
+call env\Scripts\activate
+goto pip
+
 
 :pip
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [1] Updating Pip...
+echo [2/%steps%] Updating Pip...
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 pip install -U pip
-goto :reqs
+goto reqs
+
 
 :reqs
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [2] Installing packages from requirements.txt...
+echo [3/%steps%] Installing packages from requirements.txt...
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 pip install -r requirements.txt
-goto :update
+goto update
+
 
 :update
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [3] Installing/Updating PyInstaller...
+echo [4/%steps%] Installing/Updating PyInstaller...
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 pip install -U pyinstaller
@@ -31,11 +49,10 @@ goto build
 
 
 :build
-color 7
 echo.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [4] Creating executable...
+echo [5/%steps%] Creating executable...
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 pip show pyqt5 | findstr "Location:" > "%temp%\set_var.tmp"
@@ -45,11 +62,14 @@ ren %dll_path%opengl32sw.dll opengl32sw_bak.dll
 
 pyinstaller --noconfirm --onefile --name "PyDebloatX_portable" --windowed --icon "pydebloatx/icon.ico" --add-data "pydebloatx/icon.ico;." --add-data "pydebloatx/style.css;." --add-data "pydebloatx/Language/*;Language" "pydebloatx/app.py"
 if ERRORLEVEL 1 goto errorbuild
+goto clean
 
+
+:clean
 echo.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [5] Removing build files...
+echo [6/%steps%] Removing build files...
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -58,40 +78,49 @@ del PyDebloatX_portable.spec
 
 del "%temp%\set_var.tmp"
 ren %dll_path%opengl32sw_bak.dll opengl32sw.dll
+call deactivate
 
 echo.
 echo Done.
+goto buildsuccess
 
+
+:buildsuccess
 color a
 echo.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [+] Executable built successfully and is located in dist/PyDebloatX_portable.exe
+echo [+] Executable built successfully and is
+echo     located in dist/PyDebloatX_portable.exe
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
 pause
-exit
+cd ../../
+Color 07
+exit /B 0
+
 
 :errorupdate
 color c
 echo.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo [!] There was an error while updating PyInstaller.
+echo [!] There was an error while installing/updating PyInstaller.
 echo [!] Attempting to build executable anyway.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 goto build
+
 
 :errorbuild
 color c
 del PyDebloatX_portable.spec
 rmdir /s /q "./build/"
 rmdir /s /q "./dist/"
-
 del "%temp%\set_var.tmp"
 ren %dll_path%opengl32sw_bak.dll opengl32sw.dll
+call deactivate
 echo.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -100,4 +129,7 @@ echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
 pause
-exit
+Color 07
+exit /B 1
+
+
